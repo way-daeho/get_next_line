2d37a95c8ab7388dc601f7753edc84de8d2d6187
way-daeho <lavr2755@gmail.com>
2023-12-03 22:35:13 +0900

    show lldb test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: daehlee <daehlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 15:18:16 by daehlee           #+#    #+#             */
-/*   Updated: 2023/11/18 01:21:47 by daehlee          ###   ########.fr       */
+/*   Updated: 2023/12/03 22:32:04 by daehlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,7 @@
 
 int main()
 {
-	int fd1 = open("./test.txt",O_RDONLY);
-	int fd2 = open("./test2.txt",O_RDONLY);
-	int fd3 = open("./test4.txt",O_RDONLY);
+	int fd1 = open(".testTxt/test.txt",O_RDONLY);
 	printf("%s",get_next_line(fd1));
-	printf("%s",get_next_line(fd2));
-	printf("%s",get_next_line(fd3));
-	printf("%s",get_next_line(fd1));
-	printf("%s",get_next_line(fd2));
-	printf("%s",get_next_line(fd3));
-	printf("%s",get_next_line(fd1));
-	printf("%s",get_next_line(fd2));
-	printf("%s",get_next_line(fd3));
 	return (0);
 }
